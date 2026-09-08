b9550342ff12794755a15f34478ae87408051c9f
Mehak Sharma <mehaksharma0030@gmail.com>
2026-09-09 04:19:23 +0530

    Create database_operations.sql

---
+++ 01_SQL_Basics/database_operations.sql
@@ -0,0 +1,16 @@
+
+---------------------------------
+------DATABASE OPERATIONS--------
+---------------------------------
+
+-- creating a database
+CREATE DATABASE school;
+
+-- use a database
+USE database;
+
+-- disaply all  database
+SHOW DATABASE;
+
+-- delete a database
+DROP DATABASE school;
