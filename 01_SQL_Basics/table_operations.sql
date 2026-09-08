98944603a702e11561c2d1360781c90dd465be06
Mehak Sharma <mehaksharma0030@gmail.com>
2026-09-09 04:35:45 +0530

    Create table_operations.sql

---
+++ 01_SQL_Basics/table_operations.sql
@@ -0,0 +1,48 @@
+----------------------------------
+----TABLE OPERATIONS--------------
+----------------------------------
+
+-- creating a table
+CREATE TABLE student(
+  rollNo INT PRIMARY KEY,
+  name VARCHAR(50),
+  age INT
+);
+
+-- display all tables
+SHOW TABLES;
+
+-- describe table structure/schema
+DESC student;
+
+-- add new coloum
+ALTER TABLE student
+ADD COLUMN city VARCHAR(10);
+
+-- modify an existing coloum
+ALTER TABLE student
+MODIFY COLUMN age INT NOT NULL;
+
+-- change column of the table
+ALTER TABLE student
+CHANGE name student_name VARCHAR(50);
+
+--  rename table name
+ALTER TABLE student
+RENAME TO student_details;
+
+-- delete a column in a table
+ALTER TABLE student_details
+DROP COLUMN city;
+
+-- delete all the data of the table(will preserve its structure)
+TRUNCATE TABLE student_details;
+
+-- delete the whole table (will not preserve its structure too)
+ DROP TABLE student_details;
+
+
+
+
+
+
