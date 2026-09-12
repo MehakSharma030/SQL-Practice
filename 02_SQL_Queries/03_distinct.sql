bf33741b777d9a989ade8064b058e054c2251b9b
Mehak Sharma <mehaksharma0030@gmail.com>
2026-09-12 16:46:41 +0530

    Create 03_distinct.sql

---
+++ 02_SQL_Queries/03_distinct.sql
@@ -0,0 +1,29 @@
+-- DISTINCT
+-- Used to remove duplicate values from the result
+
+
+-- 1. Select unique cities
+SELECT DISTINCT city
+FROM student;
+
+
+-- 2. Select unique ages
+SELECT DISTINCT age
+FROM student;
+
+
+-- 3. Select unique marks
+SELECT DISTINCT marks
+FROM student;
+
+
+-- 4. DISTINCT with multiple columns
+SELECT DISTINCT age, city
+FROM student;
+
+
+
+
+
+
+
