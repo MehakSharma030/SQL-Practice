b5616e2af6556c15c52a0e1f24b5f767b864c2f3
Mehak Sharma <mehaksharma0030@gmail.com>
2026-09-20 01:38:22 +0530

    Add LEFT EXCLUSIVE JOIN example with departments and employees

---
+++ 03_SQL_Joins /05_left_exclusive_join.sql
@@ -0,0 +1,50 @@
+
+-- LEFT EXCLUSIVE JOIN
+-- Returns only the rows from the LEFT table that do NOT have a matching row in the RIGHT table.
+
+
+-- Create Departments table
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+-- Insert data
+INSERT INTO departments (dept_id, dept_name) VALUES
+(1, 'IT'),
+(2, 'HR'),
+(3, 'Finance');
+
+
+-- Create Employees table
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dept_id INT,
+    salary INT
+);
+
+-- Insert data
+INSERT INTO employees (emp_id, name, dept_id, salary) VALUES
+(101, 'Mehak', 1, 60000),
+(102, 'Teena', 2, 50000),
+(103, 'Rahul', 1, 55000),
+(104, 'Aman', 4, 45000);
+
+
+-- LEFT EXCLUSIVE JOIN
+SELECT
+    employees.emp_id,
+    employees.name,
+    employees.dept_id,
+    employees.salary
+FROM employees
+LEFT JOIN departments
+ON employees.dept_id = departments.dept_id
+WHERE departments.dept_id IS NULL;
+
+
+
+
+
+
